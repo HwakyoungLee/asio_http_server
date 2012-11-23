977764ba88eaa62a37286a82141e2cec4d3cd819
root <root@SJSWT42-124.opi.com>
2012-11-24 01:33:50 +0800

    first commit

---
+++ auto.sh
@@ -0,0 +1,1 @@
+g++ -I. -I/opt/boost/include -L/opt/boost/lib -lboost_date_time -lboost_thread -lboost_system -lboost_program_options -lboost_filesystem -o server *.cpp
---
+++ client_test.sh
@@ -0,0 +1,1 @@
+echo -e "GET /mime_types.hpp HTTP/1.0\r\n\r\n"|nc 127.0.0.1 8888
